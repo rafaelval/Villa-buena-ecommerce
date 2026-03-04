b78f382e505c7deab3fdd7544a8f36aba51bd61b
rafaelval <artillero.satanas@gmail.com>
2026-03-04 18:27:54 -0500

    cambiadas rutas de api desde variables de entorno

--- src/features/products/useCategories.js
+++ src/features/products/useCategories.js
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL
+
+const CATEGORIES_URL =`${API_URL}/products/categories`
+
 const fetchCategories = async () => {
-  const { data } = await axios.get(
-    "https://dummyjson.com/products/categories"
-  );
+  const { data } = await axios.get(CATEGORIES_URL);
   return data;
 };
 
--- src/features/products/useProducts.js
+++ src/features/products/useProducts.js
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL
+
+const PRODUCTS_URL = `${API_URL}/products?limit=200`
+
 const fetchProducts = async () => {
-  const { data } = await axios.get(
-    "https://dummyjson.com/products?limit=200"
-  );
+  const { data } = await axios.get(PRODUCTS_URL);
   return data;
 };
 
